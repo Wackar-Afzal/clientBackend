adea253c2d14976655be94cb812ab763009e44a8
wackar-afzal <waqarafzalrai@gmail.com>
2022-12-08 18:05:09 +0500

    changeaaa by waqar

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cors())
 
-app.use('/api/data', GaolsRouter)
+app.use('/api/data', async(req,res)=>{
+    res.send('bund marao')
+
+})
 app.use('/', (req,res)=>res.send('hellooooo'))
 
 
